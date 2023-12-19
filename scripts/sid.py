0a1c0112084ab7565fe49582a6d7710f93dfa625
sidkipi <150452860+sidkipi@users.noreply.github.com>
2023-12-19 19:40:37 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -10,8 +10,8 @@ pattern3 = re.compile(r'^A__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$')
 matching_files = []
 unmatched_files = []
 
-# Keep track of the highest version
-highest_version = "0.0.0"
+# Keep track of the existing versions
+existing_versions = set()
 
 for file_name in os.listdir("dbscripts"):
     if file_name.endswith(".sql"):
@@ -22,11 +22,11 @@ for file_name in os.listdir("dbscripts"):
         if match2:
             version = match2.group(1)
 
-            if version <= highest_version:
+            if version in existing_versions:
                 print(f"File '{file_name}' has an equal or lower version. Exiting with an error.")
                 sys.exit(1)
             else:
-                highest_version = version
+                existing_versions.add(version)
 
         if match1 or match2 or match3:
             print(f"File '{file_name}' matches the pattern.")
