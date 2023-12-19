c9f4c24059ba99f2388dd0409629cffb7162a189
sidkipi <150452860+sidkipi@users.noreply.github.com>
2023-12-19 18:38:17 +0530

    Update sid.py

--- scripts/sid.py
+++ scripts/sid.py
@@ -2,25 +2,28 @@ import os
 import re
 import sys
 
-valid = "true"
+valid = True
 pattern1 = re.compile(r'^R__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$')
 pattern2 = re.compile(r'^[vV]\d+\.\d+\.\d+__[a-zA-Z0-9_]+\.sql$')
 pattern3 = re.compile(r'^A__[a-zA-Z]+(?:_[a-zA-Z0-9]+)+\.(sql|SQL)$')
 
 matching_files = []
+unmatched_files = []
 
 for file_name in os.listdir("dbscripts"):
     if file_name.endswith(".sql"):
         if pattern1.match(file_name) or pattern2.match(file_name) or pattern3.match(file_name):
             print(f"File '{file_name}' matches the pattern.")
             matching_files.append(file_name)
-            valid = "true"
+        else:
+            print(f"File '{file_name}' does not match the pattern.")
+            unmatched_files.append(file_name)
+            valid = False
 
-
-if valid == "true":
-    print(f"::set-output name=valid::{valid}")
-   
+if valid:
+    print("::set-output name=valid::true")
     print("Matching files:", matching_files)
 else:
-    print("No matching files found. Exiting with success.")
-    sys.exit(0)
+    print("Validation failed. Exiting with an error.")
+    print("Unmatched files:", unmatched_files)
+    sys.exit(1)
