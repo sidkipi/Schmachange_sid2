a261150ffde333a4f12fa4236500c09f20bad7b3
sidkipi <150452860+sidkipi@users.noreply.github.com>
2023-12-19 18:18:56 +0530

    Create V1.1.3__fidelity_life.sql

---
+++ dbscripts/V1.1.3__fidelity_life.sql
@@ -0,0 +1,8 @@
+use database DEMO1_DB;
+use schema PUBLIC;
+
+create table hyd_test_users if not exists(
+  name varchar (100),  -- variable string column
+  preferences string, -- column used to store JSON type of data
+  created_at timestamp
+);
